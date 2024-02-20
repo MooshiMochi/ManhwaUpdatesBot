9a4c8ff4d298922ae63ef4b4be5a6aecd8305b13
Mooshi <rchiriac16@gmail.com>
2024-02-20 22:18:07 +0000

    i pray this fixes the stupid ass bug

--- src/core/database.py
+++ src/core/database.py
@@ -776,7 +776,7 @@ class Database:
                             UNION
                             SELECT series_id, scanlator FROM tracked_guild_series
                         )
-                        AND scanlator IN (SELECT scanlator FROM scanlators_config WHERE enabled = true)
+                        AND scanlator NOT IN (SELECT scanlator FROM scanlators_config WHERE enabled = false)
                         AND lower(status) NOT IN ({completed_db_set});
                     """
             ) as cursor:
